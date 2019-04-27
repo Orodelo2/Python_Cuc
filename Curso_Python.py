6cdddfcfad78165b0e9be0e03a0c87f2298f4fc8
Orlando <rodso91@gmail.com>
2019-04-27 10:42:07 -0500

    Foto inicial

--- Curso_Python.py
+++ Curso_Python.py
@@ -9,3 +9,37 @@ parrafo
 """
 # Hola mundo
 print("Hola mundo")
+
+# Variables
+
+x = 4  # Int
+y = "Texto"  # String
+z = 4.5  # Float
+a = True  # Booleano
+b = False  # Booleano
+c = 4 + 5j  # Complejo
+print(x, y, z, a, b, c)  # Imprime el valor de cada variable
+print("x" + y + "z" + "a" + "b" + "c")  # Imprime cada variable como String
+print(type(b))  # Imprime el tipo de dato de la variable
+
+# Conversion de tipos de datos
+
+# Enteros
+x = int(2)
+print(x)
+
+x = int(2.8)
+print(x)
+
+x = int("2.8")
+print(x)
+
+# Float
+x = float(2)
+print(x)
+
+x = float("2")
+print(x)
+
+x = float("2.8")
+print(x)
